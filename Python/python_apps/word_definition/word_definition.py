3a12e9b94f413ed28f2ec3129a892379c8f70d8d
Jaden Yang <lingyaoy@stanford.edu>
2021-01-12 20:07:59 -0800

    Update word_definition.py

--- Python/python_apps/word_definition/word_definition.py
+++ Python/python_apps/word_definition/word_definition.py
@@ -6,6 +6,7 @@
 import json
 import difflib
 from difflib import SequenceMatcher
+from difflib import get_close_matches
 
 #%%
 #Load the json file which contains the word definitions
@@ -17,13 +18,30 @@ data=json.load(open('data.json'))
 #%%
 word=input("Enter a word: ")
 
-#%%
 def word_meaning(word):
     if word in data:
         word=word.lower()
         return data[word]
+    elif word.title() in data:
+        return data[word.title()]
+    elif word.upper() in data:
+        return data[word.upper()]
+    elif len(get_close_matches(word,data.keys(),cutoff=0.8))>0:
+        yn= input("Did you mean %s instead? Enter Y if yes, N if no: " % get_close_matches(word,data.keys())[0])
+        if yn=="Y":
+            return data[get_close_matches(word,data.keys())[0]]
+        elif yn=="N":
+            return "The word you entered doesn't exist in the database. Please double check it."
+        else:
+            return "We didn't understandt your entry"
     else:
         return "The word doesn't exist. Please double check"
 
-print(word_meaning(word))
+output=word_meaning(word)
+
+if type(output)==list:
+    for item in output:
+        print(item)
+else:
+    print(output)
 # %%
