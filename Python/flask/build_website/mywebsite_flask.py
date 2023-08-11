d40607c3cd8c8e5b589db663131d15ddbdb8984e
Jaden <jadentong9@gmail.com>
2023-08-10 20:32:00 -0400

    updates on website design
    1. update the flask py file and index file in order to dynamic contents from flask
    2. add some contents to note section on work page

--- Python/flask/build_website/mywebsite_flask.py
+++ Python/flask/build_website/mywebsite_flask.py
@@ -1,7 +1,6 @@
 ## This code is used to build a personal website using Flask in python
 
 from flask import Flask, render_template
-from flask.templating import render_template
 
 import plotly
 import plotly.graph_objs as go
@@ -17,9 +16,49 @@ app=Flask(__name__,template_folder='template')
 def home():
     return render_template("home.html")
 
-@app.route('/<string:page_name>')
-def html_page(page_name):
-    return render_template(page_name)
+@app.route('/work')
+def work():
+    output='give a try'
+    return render_template("work.html", outputs=output)
+
+@app.route('/travel')
+def travel():
+    return render_template("travel.html")
+
+@app.route('/blog')
+def blog():
+    return render_template("blog.html")
+
+@app.route('/contact')
+def contact():
+    return render_template("contact.html")
+
+@app.route('/test')
+def test():
+    return render_template("swiss.html")
+
+# @app.route('/travel/')
+# def travel():
+#     return render_template("pages/travel.html")
+
+
+
+# @app.route('/<string:page_name>')
+# def html_page(page_name):
+#     return render_template(page_name)
+
+
+# def get_flask_output():
+#     return "Hello, this is the output from Flask!"
+
+# @app.route('/<string:page_name>')
+# def work(page_name):
+#     output='give a try'
+#     return render_template(page_name, outputs=output)
+
+
+
+
 
 # @app.route('/swiss/')
 # def swiss():
